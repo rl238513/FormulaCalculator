7ecd108f4ff6c32f13d047a8191996a58220ce94
Eduardo Mendoza <rl238513@stu.rlas-116.org>
2019-08-22 12:43:22 -0500

    Initial Commit

---
+++ FormulaCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FormulaCalculator
+//
+//  Created by EDUARDO MENDOZA on 8/22/19.
+//  Copyright © 2019 EDUARDO MENDOZA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
